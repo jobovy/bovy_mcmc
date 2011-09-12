1d7c712f1110393cbb331a686cbd17f209d452a3
Jo Bovy <jo.bovy@gmail.com>
2011-09-12 13:55:44 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup #, Extension
+import os, os.path
+import re
+
+longDescription= ""
+
+
+setup(name='bovy_mcmc',
+      version='1.',
+      description='MCMC routines',
+      author='Jo Bovy',
+      author_email='bovy@ias.edu',
+      license='New BSD',
+      long_description=longDescription,
+      url='https://github.com/jobovy/bovy_mcmc',
+      package_dir = {'bovy_mcmc/': ''},
+      packages=['bovy_mcmc'],
+      )
